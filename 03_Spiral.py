c28289cb0028cc216ab58bc1a7ee2f4980eeae59
Noah-Maze <nbm021000@gmail.com>
2017-12-08 20:44:25 -0600

    More formatting and stylistic rewrites

--- 03_Spiral.py
+++ 03_Spiral.py
@@ -1,5 +1,74 @@
 
-def man(max_idx):
+def manhattan_sp(desired_cell):
+    ''' Return the manhattan distance
+        from Cell Number 1 for a
+        given cell in a spiral
+        arranged like so:
+
+            17  16  15  14  13
+            18   5   4   3  12
+            19   6   1   2  11
+            20   7   8   9  10
+            21  22  23---> ...
+
+        For example:
+            # manhattan_sp(1) = 0, since Cell 1 is 0 cells away from itself.
+            # manhattan_sp(12) = 3, because it is two cells 'left' and one
+                cell 'up' from Cell 1
+            # manhattan_sp(23) = 2, because it is two cells below Cell 1
+            # manhattan_sp(1024) = 31
+    '''
+    def rotate(heading):
+        ''' Rotate the heading 90 degrees CCW
+        '''
+        return (-heading[1], heading[0])
+    def hallways():
+        ''' The Spiral's hallways have a nice rhythm!
+            Starting at Cell 1, move:
+                East 1
+                North 1
+                West 2
+                South 2
+                East 3
+                North 3
+                etc
+            The direction is tracked by the called so
+            this generator just spits out integers like
+            1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
+        '''
+        i = 1
+        while True:
+            yield i
+            yield i
+            i+=1
+    # Start at Cell 1
+    cell_number = 1
+    x = 0
+    y = 0
+    # Facing East
+    heading = (1,0)
+    # Walk down longer and longer hallways
+    for hall in hallways():
+        for cell in range(hall):
+            if cell_number == desired_cell:
+                # Return the manhattan distance from Cell 1
+                return abs(x) + abs(y)
+            # Move in the direction of heading to the next cell
+            x += heading[0]
+            y += heading[1]
+            cell_number +=1
+        # Turn left at the end of the hallway
+        heading = rotate(heading)
+
+# tests
+assert manhattan_sp(1)==0
+assert manhattan_sp(12)==3
+assert manhattan_sp(23)==2
+assert manhattan_sp(1024)==31
+
+print "Answer for Part One: " + str(manhattan_sp(289326))
+
+def weird_mem(max_value):
     def rotate(heading):
         return (-heading[1], heading[0])
     def hallways():
@@ -8,49 +77,8 @@ def man(max_idx):
             yield i
             yield i
             i+=1
-    idx = 1
-    x = 0
-    y = 0
-    heading = (1,0)
-    for hall in hallways():
-        for cell in range(hall):
-            if idx == max_idx:
-                return abs(x) + abs(y)
-            x += heading[0]
-            y += heading[1]
-            idx +=1
-        heading = rotate(heading)
-
-# tests
-    # Data from square 1 is carried 0 steps, since it's at the access port.
-    # Data from square 12 is carried 3 steps, such as: down, left, left.
-    # Data from square 23 is carried only 2 steps: up twice.
-    # Data from square 1024 must be carried 31 steps.
-assert man(1)==0
-assert man(12)==3
-assert man(23)==2
-assert man(1024)==31
-
-print(man(289326))
-
-def weird_ram(max_idx, max_value):
-    def rotate(heading):
-        if heading==(1,0):
-            return (0,1)
-        if heading==(0,1):
-            return (-1,0)
-        if heading==(-1,0):
-            return (0,-1)
-        if heading==(0,-1):
-            return (1,0)
-    def hallways():
-        i = 1
-        while True:
-            yield i
-            yield i
-            i+=1
-    # keep track of written cells until they're useless
-    outer_cells = {(0,0):1} # Special case for 1st idx
+    # keep track of written cells
+    cells = {(0,0):1} # Special case for 1st cell
     value = 1
     idx = 1
     x = 0
@@ -58,15 +86,16 @@ def weird_ram(max_idx, max_value):
     heading = (1,0)
     for hall in hallways():
         for cell in range(hall):
-            print ((x, y), value)
-            if value >= max_value or idx > max_idx:
+            if value >= max_value:
                 return value
             x += heading[0]
             y += heading[1]
-            value =(outer_cells.get((x-1, y+1), 0) + outer_cells.get((x+0, y+1), 0) + outer_cells.get((x+1, y+1), 0) +
-                    outer_cells.get((x-1, y+0), 0) + outer_cells.get((x,y), 0)      + outer_cells.get((x+1, y+0), 0) +
-                    outer_cells.get((x-1, y-1), 0) + outer_cells.get((x+0, y-1), 0) + outer_cells.get((x+1, y-1), 0))
-            outer_cells[(x,y)]=value
-            # TODO: Pop cells that are useless if ram usage explodes
+            value =(cells.get((x-1, y+1), 0) + cells.get((x+0, y+1), 0) + cells.get((x+1, y+1), 0) +
+                    cells.get((x-1, y+0), 0) + cells.get((x,y), 0)      + cells.get((x+1, y+0), 0) +
+                    cells.get((x-1, y-1), 0) + cells.get((x+0, y-1), 0) + cells.get((x+1, y-1), 0))
+            cells[(x,y)]=value
+            # Potential Optimization: Forget cells that are useless (if ram usage explodes)
             idx +=1
         heading = rotate(heading)
+
+print "Answer for Part Two: " + str(weird_mem(289326))
